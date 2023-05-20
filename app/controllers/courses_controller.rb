1e228256eb6b0308bf1ec84fef8b977230d1604a
CodeIgor31 <yagmurov.igor@mail.ru>
2023-05-20 08:47:13 +0300

    Little fix

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -12,7 +12,12 @@ class CoursesController < ApplicationController
   def sign
     @course = Course.find_by(id: params[:id])
     UsersAndCourse.create(user_id: current_user.id, course_id: @course.id)
-    count_of_courses = Rating.find_by(user_id: current_user.id).all_courses
+    count = Rating.find_by(user_id: current_user.id)
+    if count != nil
+      count_of_courses = count.all_courses 
+    else
+      count_of_courses = 1
+    end
     Rating.find_by(user_id: current_user.id).update(all_courses: count_of_courses+1)
     redirect_to show_cu_path
   end
