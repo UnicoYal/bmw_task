dcade8bdd9196e979eace0d49e74171b9cd3b0fe
CodeIgor31 <yagmurov.igor@mail.ru>
2023-05-20 09:39:16 +0300

    Updated migs

--- db/migrate/20230518153559_add_status_to_courses_users.rb
+++ db/migrate/20230518153559_add_status_to_courses_users.rb
@@ -1,4 +1,4 @@
-class AddStatusToUsersAndCourses < ActiveRecord::Migration[7.0]
+class AddStatusToCoursesUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :courses_users, :status, :boolean, default: false
   end
