b1852f596e942c925baf17205585dc962e791b4e
FlyRexy <eferovnik@mail.ru>
2023-05-20 23:41:29 +0300

    lessons page finished

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_05_19_212848) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -48,8 +45,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_19_212848) do
   end
 
   create_table "courses_users", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "course_id"
+    t.integer "user_id"
+    t.integer "course_id"
     t.boolean "status", default: false
     t.index ["course_id"], name: "index_courses_users_on_course_id"
     t.index ["user_id"], name: "index_courses_users_on_user_id"
